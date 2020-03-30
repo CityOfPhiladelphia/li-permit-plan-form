8e7ffb8e6cbd12735dcc266e6a384563d584845d
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-03-30 16:28:50 -0400

    Update paths

--- etl/etl.bat
+++ etl/etl.bat
@@ -1,1 +1,1 @@
-start C:\Users\peter.dannemann\AppData\Local\Programs\Python\Python36\python.exe D:\permit-plan-form-test\li-permit-plan-form\etl\main.py
+start C:\Users\peter.dannemann\AppData\Local\Programs\Python\Python36\python.exe D:\li-permit-plan-form\etl\main.py
